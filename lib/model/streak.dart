042764b9556329d570f5064ac8a494b9878cebd1
Ian McLerran <imclerran@protonmail.com>
2023-01-03 19:56:04 -0800

    added  model class

---
+++ lib/model/streak.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
+
+@immutable
+class Streak extends Equatable with Comparable {
+  final String id;
+  final String title;
+  final String units;
+  final int streakLength;
+  final DateTime lastCompleted;
+
+  Streak({
+    String id,
+    this.title = "",
+    this.units = "Days",
+    this.streakLength = 0,
+    this.lastCompleted,
+  }) : this.id = id ?? Uuid().v4();
+
+  @override
+  int compareTo(other) {
+    // TODO: implement compareTo
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
