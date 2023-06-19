96b5a8a67091e5dd6c1a302aab5e307ed66e9937
Ian McLerran <imclerran@protonmail.com>
2023-06-19 15:40:24 -0700

    test Task.dueDateString = "Tomorrow"

--- test/model_test.dart
+++ test/model_test.dart
@@ -12,5 +12,15 @@ void main() {
       // assert
       expect(result, "Today");
     });
+    test(
+        'Task.dueDateString should return "Tomorrow" when the task is due tomorrow',
+        () {
+      // arrange
+      var task = Task(dueDate: DateTime.now().add(Duration(days: 1)));
+      // act
+      var result = task.dueDateString;
+      // assert
+      expect(result, "Tomorrow");
+    });
   });
 }
